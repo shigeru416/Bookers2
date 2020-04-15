44ebea08a1c229bf434b28776dbff597fff01aa3
松崎匡浩 <game.1andwork.1etc@gmail.com>
2020-04-16 01:27:02 +0900

    各画面作成中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,4 +6,18 @@ class BooksController < ApplicationController
 	end
 	def index
 	end
+	def new
+		@book = Book.new
+	end
+	def create
+        @book = Book.new(book_params)
+        @book.user_id = current_user.id
+        @book.save
+        redirect_to user_path(current_user.id)
+	end 
+
+	private
+	def book_params
+        params.require(:book).prermit(:title, :body)
+	end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,9 @@ class UsersController < ApplicationController
 		@user.update(user_params)
 		redirect_to user_path(@user.id)
 	end
-
+	def show
+		@user = User.find(params[:id])
+    end
 	private
 	def user_params
 		params.require(:user).permit(:name, :introduction, :profile_image)

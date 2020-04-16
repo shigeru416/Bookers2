80fe1b47cc1988b847104a98aaa8a4dd0c1b5b89
松崎匡浩 <game.1andwork.1etc@gmail.com>
2020-04-16 14:53:56 +0900

    users#show, users#index, books#show, books#index画面作成中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,8 @@ class BooksController < ApplicationController
 	def about
 	end
 	def index
+		@user = current_user
+		@book = Book.new
 	end
 	def create
         @book = Book.new(book_params)
@@ -12,6 +14,19 @@ class BooksController < ApplicationController
         @book.save
         redirect_to book_path(@book.id)
 	end 
+	def show
+		@user = current_user
+		@book = Book.new
+	end
+	def edit
+		@book = Book.find(params[:id])
+	end
+	def update
+		book = Book.find(params[:id])
+		book.update(book_params)
+		redirect_to book_path(book.id)
+	end
+
 
 	private
 	def book_params
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,10 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 		@book = Book.new
     end
+    def index
+    	@user = current_user
+    	@book = Book.new
+    end
 
 
 
